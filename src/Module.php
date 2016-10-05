eb423cd6d2236b0d4a5fc3450437d6a253e0e099
Frederic Dewinne <frederic@continuousphp.com>
2016-10-05 16:18:23 +0200

    fix path refactoring issue

--- src/Module.php
+++ src/Module.php
@@ -41,11 +41,11 @@ class Module
     {
     	$config = array();
     	$configFiles = array(
-			include __DIR__ . '/config/module.config.php',
-			include __DIR__ . '/config/app.interface.php',
-			include __DIR__ . '/config/app.forms.php',
-			include __DIR__ . '/config/diagnostic.config.php',
-            include __DIR__ . '/config/app.tools.php',
+			include __DIR__ . '/../config/module.config.php',
+			include __DIR__ . '/../config/app.interface.php',
+			include __DIR__ . '/../config/app.forms.php',
+			include __DIR__ . '/../config/diagnostic.config.php',
+            include __DIR__ . '/../config/app.tools.php',
     	);
     	
     	foreach ($configFiles as $file) {
@@ -53,17 +53,6 @@ class Module
     	} 
     	
     	return $config;
-    }
-
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
     }
     
     public function createTranslations($e)
