e227243c2965645bfaa51edbddc871320c6ba0e5
Cris <crismakabenta@gmail.com>
2019-02-08 16:01:02 +0800

    Updated plugin interface to adopt to the dashboard plugin rights

--- config/dashboard-plugins/MelisCalendarEventsPlugin.config.php
+++ config/dashboard-plugins/MelisCalendarEventsPlugin.config.php
@@ -1,34 +1,61 @@
-<?php 
-    return array(
-        'plugins' => array(
-            'meliscalendar' => array(
-                'ressources' => array(
-                    'css' => array(
-                    ),
-                    'js' => array(
-                    )
+<?php
+return array(
+    'plugins' => array(
+        'meliscore' => [
+            'interface' => [
+                'melis_dashboardplugin' => [
+                    'conf' => [
+                        'dashboard_plugin' => true
+                    ],
+                    'interface' => [
+                        'melisdashboardplugin_section' => [
+                            'interface' => [
+                                'MelisCalendarEventsPlugin' => [
+                                    'conf' => [
+                                        'type' => '/meliscalendar/interface/MelisCalendarEventsPlugin'
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+        ],
+        'meliscalendar' => array(
+            'ressources' => array(
+                'css' => array(
                 ),
-                'dashboard_plugins' => array(
-                    'MelisCalendarEventsPlugin' => array(
+                'js' => array(
+                )
+            ),
+            'interface' => [
+                'MelisCalendarEventsPlugin' => array(
+                    'conf' => [
+                        'name' => 'MelisCalendarEventsPlugin',
+                        'melisKey' => 'MelisCalendarEventsPlugin'
+                    ],
+                    'datas' => [
                         'plugin_id' => 'CalendarEvents',
                         'name' => 'tr_meliscalendar_dashboard',
                         'description' => 'tr_meliscalendar_dashboard_description',
                         'icon' => 'fa fa-calendar',
                         'thumbnail' => '/MelisCalendar/plugins/images/MelisCalendarEventsPlugin.jpg',
                         'jscallback' => 'initDashboardCalendar()',
+                        'max_lines' => 8,
                         'height' => 4,
-                        
-                        'interface' => array(
-                            'meliscalendar_events' => array(
-                                'forward' => array(
-                                    'module' => 'MelisCalendar',
-                                    'plugin' => 'MelisCalendarEventsPlugin',
-                                    'function' => 'calendarEvents'
-                                )
-                            )
-                        )
-                    )
-                )
-            )
+                        'width' => 0,
+                        'x-axis' => 0,
+                        'y-axis' => 0,
+                    ],
+                    'forward' => array(
+                        'module' => 'MelisCalendar',
+                        'plugin' => 'MelisCalendarEventsPlugin',
+                        'function' => 'calendarEvents',
+                        'jscallback' => '',
+                        'jsdatas' => array()
+                    ),
+                ),
+            ],
         )
-    );
+    )
+);
