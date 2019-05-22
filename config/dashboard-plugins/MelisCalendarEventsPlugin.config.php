9cc6095a95c4e467ac1336784577c68de5a83d54
jerremeirago <jrago@melistechnology.com>
2019-05-22 16:27:12 +0800

    changed section of plugin

--- config/dashboard-plugins/MelisCalendarEventsPlugin.config.php
+++ config/dashboard-plugins/MelisCalendarEventsPlugin.config.php
@@ -17,7 +17,19 @@
                         'thumbnail' => '/MelisCalendar/plugins/images/MelisCalendarEventsPlugin.jpg',
                         'jscallback' => 'initDashboardCalendar()',
                         'height' => 4,
-                        'section' => 'MelisCore',
+                        /*
+                         * if set this plugin will belong to a specific marketplace section,
+                           * if not it will go directly to ( Others ) section
+                           *  - available section for dashboard plugins as of 2019-05-16
+                           *    - MelisCore
+                           *    - MelisCms
+                           *    - MelisMarketing
+                           *    - MelisSite
+                           *    - MelisCommerce
+                           *    - Others
+                           *    - CustomProjects
+                         */
+                        'section' => 'MelisMarketing',
                         'interface' => array(
                             'meliscalendar_events' => array(
                                 'forward' => array(
