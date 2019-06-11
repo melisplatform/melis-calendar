640a5cc70aa886da3afce7ca456ccef974e6c413
fparan <fparan@melistechnology.com>
2019-06-11 12:38:58 +0800

    #0003888 - Translation update

--- language/fr_FR.forms.php
+++ language/fr_FR.forms.php
@@ -8,7 +8,7 @@
         'tr_melistoolcalendar_form_event_title_empty' => 'Veuillez saisir un titre avant de faire glisser l\'évènement dans le calendrier',
         'tr_melistoolcalendar_form_event_title_long' => 'Le titre de l\'évènement est trop long et doit faire moins de 150 caractères',
         // Update
-        'tr_melistoolcalendar_update_save_button' => 'Sauvegarder',
+        'tr_melistoolcalendar_update_save_button' => 'Enregistrer',
         'tr_melistoolcalendar_update_close_button' => 'Annuler',
     );
 ?>
